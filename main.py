c7b948b829c6b26473bd128fe937d5d55463aa8d
Dan Chamorro <i@danchamorro.com>
2023-02-20 21:14:31 -0500

    Added filepath argument to each function

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-def add_todo():
+def add_todo(file_path):
     """Add a todos to the todos.txt file."""
     todo = input("What do you want to add? ").strip()
-    with open("todos.txt", "a") as f:
+    with open(file_path, "a") as f:
         f.write(f"{todo}\n")
 
 
-def show_todos():
+def show_todos(file_path):
     """Show all todos in the todos.txt file."""
-    with open("todos.txt", "r") as f:
+    with open(file_path, "r") as f:
         todos = f.readlines()
     if not todos:
         print("There are no todos.")
@@ -16,9 +16,9 @@ def show_todos():
         print(f"{i + 1}: {todo.strip()}")
 
 
-def edit_todo():
+def edit_todo(file_path):
     """Edit a todo in the todos.txt file."""
-    with open("todos.txt", "r") as f:
+    with open(file_path, "r") as f:
         todos = f.readlines()
     if not todos:
         print("There are no todos to edit.")
@@ -35,13 +35,13 @@ def edit_todo():
         return
     new_todo = input("What do you want to change it to? ").strip()
     todos[todo_index - 1] = f"{new_todo}\n"
-    with open("todos.txt", "w") as f:
+    with open(file_path, "w") as f:
         f.writelines(todos)
 
 
-def complete_todo():
+def complete_todo(file_path):
     """Complete a todo in the todos.txt file."""
-    with open("todos.txt", "r") as f:
+    with open(file_path, "r") as f:
         todos = f.readlines()
     if not todos:
         print("There are no todos to complete.")
@@ -57,13 +57,13 @@ def complete_todo():
         print("Invalid todo index.")
         return
     todos[todo_index - 1] = f"X {todos[todo_index - 1]}"
-    with open("todos.txt", "w") as f:
+    with open(file_path, "w") as f:
         f.writelines(todos)
 
 
-def delete_todo():
+def delete_todo(file_path):
     """Delete a todo in the todos.txt file."""
-    with open("todos.txt", "r") as f:
+    with open(file_path, "r") as f:
         todos = f.readlines()
     if not todos:
         print("There are no todos to delete.")
@@ -79,23 +79,24 @@ def delete_todo():
         print("Invalid todo index.")
         return
     del todos[todo_index - 1]
-    with open("todos.txt", "w") as f:
+    with open(file_path, "w") as f:
         f.writelines(todos)
 
 
 def main():
+    file_path = "todos.txt"
     while True:
         user_action = input("Type add, show, edit, complete, delete or quit: ").strip()
         if user_action == "add":
-            add_todo()
+            add_todo(file_path)
         elif user_action == "show":
-            show_todos()
+            show_todos(file_path)
         elif user_action == "edit":
-            edit_todo()
+            edit_todo(file_path)
         elif user_action == "complete":
-            complete_todo()
+            complete_todo(file_path)
         elif user_action == "delete":
-            delete_todo()
+            delete_todo(file_path)
         elif user_action == "quit":
             break
         else:
