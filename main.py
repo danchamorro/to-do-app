3505469bc07b17a03a261afd4cd9fb83fec6b40c
Dan Chamorro <i@danchamorro.com>
2023-02-20 19:48:03 -0500

    Added complete functionaity. Add a check mark next to the completed item

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 def main():
     to_do_list = []
     while True:
-        user_action = input("Type add, show, edit or quit: ").strip()
+        user_action = input("Type add, show, edit, complete or quit: ").strip()
         match user_action:
             case "add":
                 to_do_list.append(input("What do you want to add? ").strip())
             case "show":
-                for index, item in enumerate(to_do_list, start=1):
+                for index, item in enumerate(to_do_list):
                     print(f"{index}. {item}")
             case "quit":
                 break
             case "edit":
                 index = int(input("Which index do you want to edit? "))
-                to_do_list[index - 1] = input("What do you want to change it to? ")
+                to_do_list[index] = input("What do you want to change it to? ")
+            case "complete":
+                index = int(input("Which index do you want to complete? "))
+                to_do_list[index] = f"✅ {to_do_list[index]}"
             case _:
                 print("Invalid action")
 
