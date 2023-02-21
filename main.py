24e50506a12b6e143af1816ca083eb0f511a4145
Dan Chamorro <i@danchamorro.com>
2023-02-20 19:30:10 -0500

    Added edit functionality

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 def main():
     to_do_list = []
     while True:
-        user_action = input("Type add or show or quit: ").strip()
+        user_action = input("Type add, show, edit or quit: ").strip()
         match user_action:
             case "add":
                 to_do_list.append(input("What do you want to add? ").strip())
             case "show":
-                print(to_do_list)
+                for index, item in enumerate(to_do_list, start=1):
+                    print(f"{index}. {item}")
             case "quit":
                 break
+            case "edit":
+                index = int(input("Which index do you want to edit? "))
+                to_do_list[index - 1] = input("What do you want to change it to? ")
             case _:
                 print("Invalid action")
 
