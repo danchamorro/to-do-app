a3f381322998d936da825abe4035c78af1f45faa
Dan Chamorro <i@danchamorro.com>
2023-02-21 15:06:50 -0500

    Created windows and button instances with PySimpleGUI.

--- gui.py
+++ gui.py
@@ -1,5 +1,10 @@
-import PySimpleGUI
+import PySimpleGUI as sg
+import cli
 
-window = PySimpleGUI.Window('My first GUI', layout=[[]])
+label = sg.Text('Type in to do')
+input_box = sg.InputText(tooltip='Type in to do')
+add_button = sg.Button('Add', tooltip='Add to do')
+
+window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]])
 window.read()
 window.close()
